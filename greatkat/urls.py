3889994a0784809dd3d16f45d1a899d6a8ab5eee
mrisho99 <mrishohamisi2348@gmail.com>
2023-03-19 04:30:24 +0300

    this is my first commit

--- greatkat/urls.py
+++ greatkat/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/',include('store.urls')),
+    path('cart/',include('carts.urls')),
     path('',views.home, name='home'),
     
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
