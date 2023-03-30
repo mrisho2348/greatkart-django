996c7fa698feb5531d5dbcb11f0c10bc7752b2e3
mrisho99 <mrishohamisi2348@gmail.com>
2023-03-30 03:18:27 +0300

    product ,order and payment

--- greatkat/urls.py
+++ greatkat/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('store/',include('store.urls')),
     path('cart/',include('carts.urls')),
     path('accounts/',include('accounts.urls')),
+    path('orders/',include('orders.urls')),
     path('',views.home, name='home'),
     
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
